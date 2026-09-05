8c5ef2c0e9ee61c1b3783522463e8873c0bcd52e
abul-hasan-farrukh <abulhasanfarrukh@gmail.com>
2026-09-05 20:28:58 +0530

    add index.js for centralized exports of TodoContext, TodoProvider, and useTodo

---
+++ 11todocontextLocal/src/contexts/index.js
@@ -0,0 +1,4 @@
+// index.js file is often created in many folder to take all 
+// the imports in one file and then use these imports from this file whereever needed.
+
+export {TodoContext, TodoProvider, useTodo} from "./TodoContext"
